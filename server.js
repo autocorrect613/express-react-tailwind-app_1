163cc05d6d1f3cd75d643e42ca7c5611128ded78
autocorrect613 <102951239+autocorrect613@users.noreply.github.com>
2022-04-11 05:15:08 -0700

    s

--- server.js
+++ server.js
@@ -3,10 +3,14 @@ const app = express();
 
 const buildPath = "./dist/";
 app.use(express.static(buildPath));
+app.use(express.static(buildPath + "static/"));
+
 app.set('port', (process.env.PORT || 5000));
 
 app.get("/", (req, res) => {
-  res.sendFile(`${buildPath}/index.html`);
+  res.sendFile(`${buildPath}/index.html`, (err) => {
+    res.json({"statuss": "neizdevās pārsūtīt failu bļeģ"})
+  });
 })
 
 app.listen(app.get('port'));
