44d633b7ee8bc2f9fc68d895abca1bb7dd865936
autocorrect613 <102951239+autocorrect613@users.noreply.github.com>
2022-04-11 03:45:25 -0700

    a

--- server.js
+++ server.js
@@ -1,13 +1,12 @@
 const express = require("express");
 const app = express();
-const path = require("path");
 
-const buildPath = "./frontend/build";
-app.use(express.static(path.join(__dirname, buildPath)));
+const buildPath = "../frontend/build";
+app.use(express.static(buildPath));
 
 app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, buildPath, "index.html"));
+  res.sendFile(`${buildPath}/index.html`);
 })
 
 const PORT = process.env.PORT || 3001;
-app.listen(PORT);
+app.listen(PORT, () => console.log("Listening to port ", PORT));
