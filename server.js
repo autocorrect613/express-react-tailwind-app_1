54c20ea6cf10d09005ad6115f468f485d0f40cbe
autocorrect613 <102951239+autocorrect613@users.noreply.github.com>
2022-04-11 04:13:49 -0700

    heroku ready

--- server.js
+++ server.js
@@ -1,11 +1,12 @@
 const express = require("express");
 const app = express();
+const path = require("path");
 
-const buildPath = "./frontend/build";
-app.use(express.static(buildPath));
+const buildPath = "/frontend/build";
+app.use(express.static(path.join(__dirname, buildPath)));
 
 app.get("/", (req, res) => {
-  res.sendFile(`${buildPath}/index.html`);
+  res.sendFile(path.join(__dirname, buildPathm, "index.html"));
 })
 
 const PORT = process.env.PORT || 3001;
